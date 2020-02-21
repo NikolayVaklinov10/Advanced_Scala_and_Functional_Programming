7a784eacbb83f647b5e63393c79ffe7bc6ab6db7
Nick <nikolayvaklinov@yahoo.com>
2020-02-21 18:37:59 +0000

    simplified version of the previous code

--- src/main/scala/lectures/part1AdvancedScala/AdvancedPatternMatching.scala
+++ src/main/scala/lectures/part1AdvancedScala/AdvancedPatternMatching.scala
@@ -45,23 +45,21 @@ object AdvancedPatternMatching extends App {
    */
 
   object even {
-    def unapply(arg: Int): Option[Boolean] =
-      if (arg % 2 == 0) Some(true)
-      else None
+    def unapply(arg: Int): Boolean = arg % 2 == 0
+
   }
 
   object singleDigit {
-    def unapply(arg: Int): Option[Boolean] =
-      if (arg > -10 && arg < 10) Some(true)
-      else None
+    def unapply(arg: Int): Boolean = arg > -10 && arg < 10
+
   }
 
 
 
   val n: Int = 8
   val mathProperty = n match {
-    case singleDigit(_) => "single digit"
-    case even(_) => "an even number"
+    case singleDigit() => "single digit"
+    case even() => "an even number"
     case _ => "no property"
   }
 
